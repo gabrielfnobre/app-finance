065e79585d3c37d99f8ddfa871b653bf16b56082
gabriel <nobrecoder@gmail.com>
2024-06-07 13:05:34 -0300

    07/06/2024

---
+++ src/br/com/nobrecoder/fin_interface/Aula01_FinInterface.java
@@ -0,0 +1,15 @@
+package br.com.nobrecoder.fin_interface;
+
+import br.com.nobrecoder.operations.Aula01_Operations;
+
+//Note que apesar de estarmos fora do módulo app-calculator é possível usar suas classes e métodos!
+
+public class Aula01_FinInterface {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Aula01_Operations.sum(2.5, 2.5, 2.5));
+		
+	}
+	
+}
--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,22 @@
-module app.finance {
+/* 
+ * COMO USAR EXPORTS E REQUIRES NOS MÓDULOS JAVA
+ * 
+ * Veja a explicação dos comandos que utilizamos abaixo para os nossos requires e exports dentro dos
+ * nossos módulos para fazer a integração por através do modulepath.
+ * 
+ * Detalhe importante, um módulo que recebe pacotes de outro módulo
+ * 
+ * INFORMAÇÃO IMPORTANTE!!!
+ * Não vamos conseguir usar os exports e requires adequadamente se não fizermos a inclusão dos módu-
+ * los que desejamos usar no modulepath do nosso projeto.
+ * 
+ * */
+
+module app.finance { //o bloco module serve para identificar o nosso módulo pelo nome e para conter nossos requires e exports dentro dele;
+	requires java.base; //O java.base (módulo que tem as principais funções do java) não precisa ser requerido, mas fizemos a título de exemplo;
+	requires app.calculator; //Aqui fizemos a requisição de todos os pacotes disponíveis dentro do módulo; 
 }
+
+/* Vá até o module-info do pacote "app-calculator" para ver como fizemos a implementação do módulo neste pacote, para ver seus requires e expor-
+ * ts;
+ */
