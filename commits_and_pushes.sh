9ab4e5477f641beb15ed7321b870e293c0c52278
Gabriel <nobrecoder@gmail.com>
2024-06-09 09:07:28 -0300

    commit in

--- commits_and_pushes.sh
+++ commits_and_pushes.sh
@@ -26,8 +26,8 @@ commit_and_push() {
 
 # Iterar sobre os diretórios dos projetos e executar commit e push
 for project_dir in "${PROJECT_DIRS[@]}"; do
-    echo "Processando $project_dir"
+    echo "\n\nProcessando $project_dir"
     commit_and_push "$project_dir"
 done
 
-echo "Commit e push completados para todos os repositórios."
+echo "\n\nCommit e push completados para todos os repositórios."
